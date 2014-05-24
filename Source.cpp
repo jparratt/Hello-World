0a4a890c09409cb280d8db5fa5109e2c4ff47a56
Jake Parratt <jparratt@gmail.com>
2014-05-23 23:04:39 -0600

    First Commit

---
+++ Source.cpp
@@ -0,0 +1,24 @@
+// Source.cpp
+// Assignment 3
+// Jake Parratt
+// CS 2450
+// Date last modified: 05/18/2014
+// I did not copy any of the following code, except what was provided by the instructor
+// -----------------------------------------------------------------------------------------
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+
+
+	// Keeps the window open
+	system("PAUSE");
+	return 0;
+}
